22e02055e09cea55958bc629eb61d03cc04fbb67
Shubham Paul <61275458+shuzzzzam@users.noreply.github.com>
2024-10-11 01:05:24 +0530

    Delete onboarding directory

--- onboarding/src/main/java/com/example/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.models.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    
-}
--- onboarding/src/main/java/com/example/repository/VehicleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.models.Vehicle;
-
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-    
-}
